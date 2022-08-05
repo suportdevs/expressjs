6439fca96ca23594905bfbfcfd88c3b6b268dbaf
suportdevs <suportdevs@gmail.com>
2022-08-05 10:29:39 +0600

    learning express function of expressjs

--- express/index.js
+++ express/index.js
@@ -1,18 +1,32 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
 
-app.use(express.static(`${__dirname}/public/`));
+// app.use(express.json());
+// app.use(express.raw());
+// app.use(express.urlencoded());
 
-app.get('/', (req, res) => {
-    res.send('This is Home Page with get request.');
+// app.use(express.static(__dirname + '/public', {
+//     index: 'home.html'
+// }));
+
+const router = express.Router({
+    caseSensitive: true
+});
+
+app.use(router)
+
+// app.get('/', (req, res) => {
+//     res.send('This is Home Page with get request.');
+// })
+// app.post('/', (req, res) => {
+//     console.log(__dirname)
+//     res.send('This is Home Page with post request.');
+// })
+        
+router.get('/about', (req, res) => {
+    res.send('This is About Page with get request.');
 })
-app.post('/', (req, res) => {
-    console.log(__dirname)
-    res.send('This is Home Page with post request.');
-})
-
 app.listen(3000, () => {
-    console.log(__dirname)
     console.log("Listening to port 3000");
 });
